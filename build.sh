a6945af3f7ae9e6bb030bfdb2fef362069e3842c
Stefan Jung <stefan.jung@mailbox.org>
2024-01-20 19:50:17 +0100

    Continue

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 
 # Not sure where JDK is installed? Call:
 # update-alternatives --list java
+
+# Compile Fluenta
 JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64; export JAVA_HOME
 echo "Set Java:"
 echo $JAVA_HOME
 ant fluenta
+
+# Echo Fluenta version
+Fluenta/fluenta_cli.sh -version
+
+# Create a project
